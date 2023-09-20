aba522d04d9c198382f766a830a4f9fc6e18728b
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-09-20 16:03:21 +0530

    Started adding basic redux setup with toolkit

---
+++ src/Redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import { authSliceReducer } from "./Slice/AuthSlice";
+const store = configureStore({
+    reducer: {
+        auth: authSliceReducer
+    },
+    devTools: true
+})
+
+export default store;
