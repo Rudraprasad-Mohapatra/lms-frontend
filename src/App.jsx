3282579d9d3cf24e9ebd73e910a3fa4c3bfd1973
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-10-03 18:30:22 +0530

    Added Access Denied page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes } from "react-router-dom"
 import AboutUs from "./Pages/AboutUs"
 import Contact from "./Pages/Contact";
 import CourseList from "./Pages/Course/CourseList";
+import Denied from "./Pages/Denied";
 import HomePage from "./Pages/HomePage";
 import Login from "./Pages/Login";
 import NotFound from "./Pages/NotFound";
@@ -20,6 +21,7 @@ function App() {
         <Route path="/signup" element={<Signup />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/denied" element={<Denied />}></Route>
 
         <Route path="*" element={<NotFound />} ></Route>
       </Routes>
