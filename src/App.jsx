4511871defcc975a676a70cdf8871334e4b4ed3a
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-10-06 00:34:12 +0530

    Added require auth

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "./App.css";
 
 import { Route, Routes } from "react-router-dom"
 
+import RequireAuth from "./Components/Auth/RequireAuth";
 import AboutUs from "./Pages/AboutUs"
 import Contact from "./Pages/Contact";
 import CourseDescription from "./Pages/Course/CourseDescription";
 import CourseList from "./Pages/Course/CourseList";
+import CreateCourse from "./Pages/Course/CreateCourse";
 import Denied from "./Pages/Denied";
 import HomePage from "./Pages/HomePage";
 import Login from "./Pages/Login";
@@ -18,12 +20,17 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} ></Route>
         <Route path="/about" element={<AboutUs />} ></Route>
-        <Route path="/courses" element={< CourseList/>} ></Route>
+        <Route path="/courses" element={< CourseList />} ></Route>
         <Route path="/signup" element={<Signup />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
         <Route path="/denied" element={<Denied />}></Route>
         <Route path="/course/description" element={<CourseDescription />}></Route>
+
+
+        <Route element={<RequireAuth allowedRoles={["ADMIN"]} />}>
+          <Route path="/course/create" element={<CreateCourse />}></Route>
+        </Route>
 
         <Route path="*" element={<NotFound />} ></Route>
       </Routes>
---
+++ src/Pages/Course/CreateCourse.jsx
@@ -0,0 +1,10 @@
+function CreateCourse() {
+    console.log("I am cratecourse.")
+    return (
+        <>
+            Create Course
+        </>
+    )
+}
+
+export default CreateCourse;
