0c174fece4010282119bf0ab70d75c3bf7c524bb
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-10-05 09:11:18 +0530

    Prepared Course Details Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom"
 
 import AboutUs from "./Pages/AboutUs"
 import Contact from "./Pages/Contact";
+import CourseDescription from "./Pages/Course/CourseDescription";
 import CourseList from "./Pages/Course/CourseList";
 import Denied from "./Pages/Denied";
 import HomePage from "./Pages/HomePage";
@@ -22,6 +23,7 @@ function App() {
         <Route path="/login" element={<Login />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
         <Route path="/denied" element={<Denied />}></Route>
+        <Route path="/course/description" element={<CourseDescription />}></Route>
 
         <Route path="*" element={<NotFound />} ></Route>
       </Routes>
--- src/Components/CourseCard.jsx
+++ src/Components/CourseCard.jsx
@@ -4,7 +4,7 @@ function Coursecard({data}) {
     // console.log(`Data is` ,data);
     const navigate = useNavigate();
     return (
-        <div onClick={() => navigate("/course/description/")} className="text-white w-[22rem] h-[430px] shadow-lg rounded-lg cursor-pointer group overflow-hidden bg-zinc-700">
+        <div onClick={() => navigate("/course/description/", {state: {...data}})} className="text-white w-[22rem] h-[430px] shadow-lg rounded-lg cursor-pointer group overflow-hidden bg-zinc-700">
             <div className="overflow-hidden">
                 <img src={data?.thumbnail?.secure_url} alt="course thumbnail" className="h-48 w-full rounded-tl-lg rounded-tr-lg group-hover:scale=[1,2] transition-all ease-in-out duration-300" />
                 <div className="p-3 space-y-1 text-white">
