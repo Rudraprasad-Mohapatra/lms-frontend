a6f8757f8a7d8875ef553ff883f8f6425cd6c974
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-12-16 01:55:53 +0530

    Stat slice created and AdminDashboard started creating App.jsx updated for registering of Admin Dashboard

---
+++ src/Pages/Dashboard/AdminDashboard.jsx
@@ -0,0 +1,13 @@
+import HomeLayout from "../../Layouts/HomeLayout";
+function AdminDashboard() {
+
+
+    
+    return (
+        <HomeLayout>
+
+        </HomeLayout>
+    )
+}
+
+export default AdminDashboard;
