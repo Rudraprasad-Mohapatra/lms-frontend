7c486691582a9855247fd03777ded54fc8f2e1b1
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-12-30 23:58:41 +0530

    Setting up environment variables for production

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,7 +1,14 @@
 import axios from "axios";
 
-// const BASE_URL = "http://localhost:5014/api/v1";
-const BASE_URL = "https://eduverse-backend-webapp.onrender.com/api/v1";
+const BASE_URL_DEVELOPMENT = import.meta.env.VITE_REACT_APP_BASE_URL_DEVELOPMENT;
+
+const BASE_URL_PRODUCTION = import.meta.env.VITE_REACT_APP_BASE_URL_PRODUCTION;
+
+const BASE_URL = import.meta.env.MODE === "production" ? BASE_URL_PRODUCTION : BASE_URL_DEVELOPMENT;
+
+const mode = import.meta.env.MODE;
+
+console.log("This is ", mode,BASE_URL_DEVELOPMENT, BASE_URL_PRODUCTION)
 
 const axiosInstance = axios.create();
 
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
 import { defineConfig } from 'vite'
+
+// Load environment variables from .env
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
