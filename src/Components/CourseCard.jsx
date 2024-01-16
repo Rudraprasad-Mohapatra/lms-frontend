251cb06c0ee383c158a280d7d06e3c4b1de82a2d
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2024-01-16 11:18:07 +0530

    Course Card responsiveness fixed

--- src/Components/CourseCard.jsx
+++ src/Components/CourseCard.jsx
@@ -4,9 +4,9 @@ function Coursecard({data}) {
     // console.log(`Data is` ,data);
     const navigate = useNavigate();
     return (
-        <div onClick={() => navigate("/course/description/", {state: {...data}})} className="text-white w-[22rem] h-[430px] shadow-lg rounded-lg cursor-pointer group overflow-hidden bg-zinc-700">
+        <div onClick={() => navigate("/course/description/", {state: {...data}})} className="group text-white w-[26rem] min-h-[430px] max-h-[auto] shadow-lg rounded-lg cursor-pointer overflow-hidden bg-zinc-700">
             <div className="overflow-hidden">
-                <img src={data?.thumbnail?.secure_url} alt="course thumbnail" className="h-48 w-full rounded-tl-lg rounded-tr-lg group-hover:scale=[1,2] transition-all ease-in-out duration-300" />
+                <img src={data?.thumbnail?.secure_url} alt="course thumbnail" className="h-48 w-full object-fit rounded-tl-lg rounded-tr-lg group-hover:scale-110 transition-all ease-in-out duration-300" />
                 <div className="p-3 space-y-1 text-white">
                     <h2 className="text-xl font-bold text-yellow-500 line-clamp-2">
                         {data?.title}
