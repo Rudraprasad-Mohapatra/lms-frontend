9fd49c3a50835ba7b1ce8b6bea1792d9b171bb12
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2024-01-18 23:34:23 +0530

    RazorPaySlice.js updated for bar chat default data show

--- src/Redux/Slice/RazorPaySlice.js
+++ src/Redux/Slice/RazorPaySlice.js
@@ -9,7 +9,7 @@ const initialState = {
     isPaymentVerified: false,
     allPayments: {},
     finalMonthSales: {},
-    monthlySalesRecord: []
+    monthlySalesRecord: [10,20,30,40,50,60,70,80,90,110,120]
 }
 
 export const getRazorpayId = createAsyncThunk("/razorpay/getId", async () => {
