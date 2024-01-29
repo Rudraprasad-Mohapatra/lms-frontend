4cf6d458b1701b445385efc7e1c243dba8407bff
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2024-01-30 00:11:26 +0530

    Added Responsive infinite carousel/slider using React, Tailwind CSS & CSS

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,32 +1,58 @@
-import { useEffect, useState } from 'react';
+import "./Carousel.css"
+
+import { useEffect, useRef, useState } from 'react';
 
 import celebrities from '../Constants/CelebrityData.js';
 import CarouselSlide from './CarouselSlide';
 
+const featuredImagesObject = celebrities;
+const productsLength = featuredImagesObject.length;
+let count = 0;
+let slideInterval;
+
 function Carousel() {
-    const [currentSlide, setCurrentSlide] = useState(1);
-    const totalSlides = celebrities.length;
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const slideRef = useRef();
 
     useEffect(() => {
-        const intervalId = setInterval(() => {
-            const nextSlide = currentSlide % totalSlides + 1;
-            setCurrentSlide(nextSlide);
-            console.log("nextSlide value is ", nextSlide);
-        }, 3000);
+        startSlider();
+        slideRef.current.addEventListener("animationend", removeAnimation);
 
-        return () => clearInterval(intervalId);
-    }, [currentSlide, totalSlides]);
+        return () => {
+            clearInterval(slideInterval);
+        }
+    }, []);
+
+    function removeAnimation() {
+        slideRef.current.classList.remove("fade-anim");
+    }
+
+    function handleOnPrevClick() {
+        clearInterval(slideInterval);
+        count = (currentIndex + productsLength - 1) % productsLength;
+        setCurrentIndex(count);
+        slideRef.current.classList.add("fade-anim");
+        startSlider();
+    }
+
+    function handleOnNextClick() {
+        clearInterval(slideInterval);
+        count = (count + 1) % productsLength;
+        setCurrentIndex(count);
+        slideRef.current?.classList?.add("fade-anim");
+        startSlider();
+    }
+
+    function startSlider() {
+        slideInterval = setInterval(() => {
+            handleOnNextClick()
+        }, 3000);
+    }
 
     return (
         <div className="carousel w-3/4 md:w-1/2 my-16 m-auto">
-            {celebrities && celebrities.map((celebrity, index) => (
-                <CarouselSlide
-                    {...celebrity}
-                    key={index + 1}
-                    slideNumber={index + 1}
-                    totalSlides={totalSlides}
-                />
-            ))}
+            <CarouselSlide slideRef={slideRef} featuredImageObject={featuredImagesObject[currentIndex]} handleOnNextClick={handleOnNextClick} handleOnPrevClick={handleOnPrevClick} />
         </div>
     );
 }
--- src/Components/CarouselSlide.jsx
+++ src/Components/CarouselSlide.jsx
@@ -1,15 +1,13 @@
-function CarouselSlide({ image, title, description, slideNumber, totalSlides }) {
-    const slideId = `slide${slideNumber}`;
-
+function CarouselSlide({ slideRef, featuredImageObject, handleOnPrevClick, handleOnNextClick }) {
     return (
-        <div id={slideId} className="carousel-item relative w-full flex">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-                <img src={image} className="w-40 rounded-full border-2 border-gray-400" />
-                <p className="text-xl text-gray-200 text-center">{description}</p>
-                <h3 className="text-2xl font-semibold">{title}</h3>
+        <div className="carousel-item relative w-full flex">
+            <div ref={slideRef} className="flex flex-col items-center justify-center gap-4 px-[15%]">
+                <img src={featuredImageObject.image} className="w-40 rounded-full border-2 border-gray-400" />
+                <p className="text-xl text-gray-200 text-center">{featuredImageObject.description}</p>
+                <h3 className="text-2xl font-semibold">{featuredImageObject.title}</h3>
                 <div className="absolute flex gap-x-4 justify-between transform -translate-y-1/2 sm:top-[30%] md:top-1/2 w-full px-2">
-                    <a href={`#slide${slideNumber === 1?totalSlides:slideNumber - 1}`} className="btn btn-circle">❮</a>
-                    <a href={`#slide${slideNumber === totalSlides?1:slideNumber + 1}`} className="btn btn-circle">❯</a>
+                    <button onClick={handleOnPrevClick} className="btn btn-circle">❮</button>
+                    <button onClick={handleOnNextClick} className="btn btn-circle">❯</button>
                 </div>
             </div>
         </div>
